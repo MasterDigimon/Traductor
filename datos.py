e5e9f2e2625524d3446601d0cf5946999d8a6783
Jesus Eduardo Quintero Gomez <71619810+MDigimon@users.noreply.github.com>
2022-09-28 14:59:43 -0500

    Primer commit

---
+++ datos.py
@@ -0,0 +1,15 @@
+
+nems = {"ADCA":["2-89", "2-99", "3-B2"], "ADCB":["2-C9", "2-D9", "3-F9"], "ADDA":["2-8B", "2-9B", "3-BB"], "ADDB":["2-CB", "2-DB", "3-FB"], "ADDD":["2-C3", "2-D3", "3-F3"], "ANDA":["2-84", "2-94", "3-B4"], "ANDB":["2-C4", "2-D4", "3-F4"], "ANDCC":["2-10"], "ASL":[None, None, "3-78"], "ASR":[None, None, "3-77"], "BCLR":[None, "3-4D", "4-1D"], "BITA":["2-85", "2-95", "3-B5"]}
+inherentes = {"ABA": "1806", "ASLA" : "48", "ASLB": "58", "ASLD": "59", "ASRA":"47", "ASRB":"57", "BGND":"00"}
+rels = {"BCC":"24", "BCS":"25", "BEQ":"27", "BGE":"2C", "BGT":"2E", "BHI":"22", "BHS":"24"}
+
+class Linea:
+    def __init__(self, _palabra, _parametro, _direccionamiento, _error, _direccion, codigo):
+        self.palabra = _palabra
+        self.parametro = _parametro
+        self.direccionamiento = _direccionamiento #Falso / Verdadero
+        self.error = _error
+        self.direccion = _direccion
+        self.codigo = codigo
+
+
---
+++ main.py
@@ -0,0 +1,237 @@
+import os
+import string
+from datos import *
+
+
+def main():
+    f = True
+    etiquetas = []
+    while(f):
+        os.system("cls")
+        name = leer_archivo()
+        try:
+            archivo = open(name, "r")
+            primera_lectura(archivo)
+            
+        except FileNotFoundError:
+            print("No se ha encontrado el archivo")
+            print("Inserte s para salir, cualquier otra cosa para continuar")
+            option = input()
+            if(option == "s"):
+                f = False
+    
+
+
+    pass
+
+def leer_archivo():
+    name = ""
+    print("Inserte el nombre del archivo:", end=" ")
+    name = input()
+    return name
+
+
+
+def primera_lectura(archivo):
+    etiquetas = {}
+    direccion = None
+    palabra = ""
+    parametro = ""
+    cont = 0
+    cont_lineas = 0
+    lineas = []
+    nemonico = []
+    otro = None
+
+    for linea in archivo:
+        for ch in linea:
+            if(ch == " " and cont == 1):
+                cont = 2
+            elif(ch != " " and cont == 0):
+                cont += 1
+                palabra += ch
+            elif(ch == ":" and cont == 1):
+                etiquetas[palabra] = direccion
+                palabra = ""
+                cont = 0
+            elif(ch != " " and cont == 1):
+                palabra += ch
+            elif(ch != " " and cont == 2):
+                parametro += ch
+
+        if(parametro != ""):
+            if(parametro in etiquetas): #Etiquetas como parametro
+                parametro = etiquetas[parametro]
+                pass
+            else:
+                x = ident_parametro(parametro)
+                if(x == False):
+                    pass
+                    
+                elif(palabra == "ORG"):
+                    direccion = x
+                    lineas[cont_lineas] = Linea(palabra, x, False, None, direccion, None)
+
+                else:
+                    if(palabra in nems):
+                        nemonico = nems[palabra]
+                        if(x[0] == "#"):
+                            if(nemonico[0] != None):
+                                dato = nemonico[0]
+                                direccion = suma_direcciones(direccion, dato[0])
+                                lineas[cont_lineas] = Linea(palabra, x, True, None, direccion, dato[2-len(dato)]) #Guarda IMM
+
+
+                        elif(len(x) <= 2):
+                            if(nemonico[1] != None):
+                                dato = nemonico[1]
+                                direccion = suma_direcciones(direccion, dato[0])
+                                lineas[cont_lineas] = Linea(palabra, x, False, None, direccion, dato[2:len(dato)]) #Guarda DIR
+
+                            elif(nemonico[2] != None):
+                                dato = nemonico[2]
+                                direccion = suma_direcciones(direccion, dato[0])
+                                lineas[cont_lineas] = Linea(palabra, x, False, None, direccion, dato[2:len(dato)]) #Guarda EXT
+
+                            else: #ERROR 
+
+                                pass
+                        
+                        elif(len(x) <= 4):
+                            if(nemonico[2] != None):
+                                dato = nemonico[2]
+                                direccion = suma_direcciones(direccion, dato[0])
+                                lineas[cont_lineas] = Linea(palabra, x, False, None, direccion, dato[2:len(dato)]) #Guarda EXT
+
+                            elif(nemonico[1] != None):
+                                dato = nemonico[1]
+                                direccion = suma_direcciones(direccion, dato[0])
+                                lineas[cont_lineas] = Linea(palabra, x, False, "Fuera de Rango", direccion, dato[2:len(dato)]) #Guarda DIR con ERROR
+
+                            else: #ERROR
+
+                                pass
+
+
+                    elif(palabra in inherentes):
+                        direccion = suma_direcciones(direccion, len(inherentes[palabra]) / 2)
+                        lineas[cont_lineas] = Linea(palabra, x, False, "Parametro Invalido", direccion, inherentes[palabra]) #Guarda INH con error
+
+                    elif(palabra in rels):  #
+                        direccion = suma_direcciones(direccion, 2)
+                        if(len(x) <= 4):
+                            lineas[cont_lineas] = Linea(palabra, x, False, None, direccion, rels[palabra]) #Guard REL
+                        
+                        else:
+                            lineas[cont_lineas] = Linea(palabra, x, "Fuera de Rango", None, direccion, rels[palabra]) #Guarda REL con ERROR
+
+
+        elif(palabra == "ORG"):
+            lineas[cont_lineas] = Linea(palabra, parametro, False, "Parametro Invalido", direccion, None)
+            pass
+
+        elif(palabra == "END"):
+
+            pass
+
+        cont_lineas += 1
+        palabra = ""
+        parametro = ""
+        cont = 0
+            
+    pass
+
+def busqueda_nem():
+    pass
+
+def ident_parametro(parametro):
+    n_parametro = ""
+    if(parametro[0] == "#"): #Comprueba IMM
+        n_parametro += "#"
+        parametro = parametro.replace("#", "")
+
+    if(parametro[0] == "$"): #Comprueba Hexadecimales
+        num = comp_hex(parametro)
+        if(num == False):
+            return False
+        
+        n_parametro += num
+
+    elif(parametro[0] == "@"): #Comprueba Octales
+        num = oct_hex(parametro)
+        if(num == False):
+            return False
+        
+        n_parametro += num
+
+    elif(parametro[0] == "%"): #Comprueba Binarios
+        num = bin_hex(parametro)
+        if(num == False):
+            return False
+        
+        n_parametro += num
+
+    else: #Comprueba Decimales
+        num = dec_hex(parametro)
+        if(num == False):
+            return False
+        
+        n_parametro += num
+
+
+
+    return n_parametro
+
+def oct_hex(octal:string):
+    nuevo = octal.replace("@", "")
+
+    for ch in nuevo:
+        if not(ch in "01234567"):
+            return False
+    
+    num = int(nuevo, 8)
+    num = hex(num)
+    num = num.replace("0x", "")
+    return num.upper()
+
+def comp_hex(hex:string):
+    nuevo = hex.replace('$', '')
+
+    try:
+        num = int(nuevo, 16)
+        return nuevo
+
+    except:
+        print("Error de conversion")
+        return False
+    
+def bin_hex(bin:string):
+    nuevo = bin.replace("%", "")
+
+
+    for ch in nuevo:
+        if not(ch in "01"):
+            return False
+    
+    num = int(nuevo, 2)
+    num = hex(num)
+    num = num.replace("0x", "")
+    return num.upper()
+
+def dec_hex(dec:string):
+    for ch in dec:
+        if not(ch in "0123456789"):
+            return False
+    
+    num = hex(int(dec))
+    num = num.replace("0x", "")
+    return num.upper()
+
+def suma_direcciones(dir1, dir2):
+    dirX = int(dir1, 16) + int(dir2)
+    dirY = hex(dirX)
+    dirZ = dirY.replace("0x", "")
+    return dirZ.upper()
+
+
+main()
